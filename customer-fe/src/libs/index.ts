8af5218664b2cb2546b29adcd0cd76623eed8005
chuong1210 <chuongbd1012@gmail.com>
2025-01-07 20:37:15 +0700

    config export

--- customer-fe/src/libs/index.ts
+++ customer-fe/src/libs/index.ts
@@ -1,2 +1,2 @@
 export * from './cookies';
-export * from './util';
+export * from './request';
--- customer-fe/src/redux/index.ts
+++ customer-fe/src/redux/index.ts
@@ -1,13 +1,27 @@
-
-import { configureStore} from '@reduxjs/toolkit';
+import { configureStore, type Action, type ThunkAction } from '@reduxjs/toolkit';
+import {
+	useDispatch as useReduxDispatch,
+	useSelector as useReduxSelector,
+	type TypedUseSelectorHook,
+} from 'react-redux';
+import { reduxMiddlewares } from '../middleware/redux-middleware';
+import {  tokenExpiresSlice } from './slices';
 
 const reducer = {
+	tokenExpires: tokenExpiresSlice.reducer,
 
 };
 
 export const reduxStore = configureStore({
 	reducer,
 	middleware: (getDefaultMiddleware) => {
-
+		return getDefaultMiddleware().concat(reduxMiddlewares);
 	},
 });
+
+export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
+export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
+export type ReduxStore = typeof reduxStore;
+export type ReduxState = ReturnType<typeof reduxStore.getState>;
+export type ReduxDispatch = typeof reduxStore.dispatch;
+export type ReduxThunkAction<ReturnType = void> = ThunkAction<ReturnType, ReduxState, unknown, Action>;
